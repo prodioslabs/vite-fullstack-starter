db0f07e2be9bc3b3d5e559d17310ebc32167c4f5
Abinash Panda <abinash@prodios.com>
2024-04-05 12:48:08 +0530

    fix: move QueryClient from main.tsx -> app.tsx

--- apps/client/src/app.tsx
+++ apps/client/src/app.tsx
@@ -2,6 +2,7 @@ import { RouterProvider, createRouter } from '@tanstack/react-router'
 
 // Import the generated route tree
 import { Toaster } from '@repo/ui'
+import { QueryClientProvider } from '@tanstack/react-query'
 import { routeTree } from './routeTree.gen'
 import { queryClient } from './lib/client'
 
@@ -17,9 +18,9 @@ declare module '@tanstack/react-router' {
 
 export default function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
       <Toaster />
-    </>
+    </QueryClientProvider>
   )
 }
--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,14 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { QueryClientProvider } from '@tanstack/react-query'
 import './styles/globals.css'
 import App from './app'
-import { queryClient } from './lib/client'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <App />
   </React.StrictMode>,
 )
