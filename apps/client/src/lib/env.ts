23008af61bfce06b71464d7e7d6d264ea47a4c85
Trugamr <trugamr@live.in>
2024-06-17 20:15:32 +0530

    feat(client): allow relative base url

--- apps/client/src/lib/env.ts
+++ apps/client/src/lib/env.ts
@@ -4,7 +4,10 @@ import { z } from 'zod'
 export const env = createEnv({
   clientPrefix: 'VITE_',
   client: {
-    VITE_API_BASE_URL: z.string().url().min(1),
+    VITE_API_BASE_URL: z.string().transform((value) => {
+      // Allow relative or absolute URLs but convert them to absolute URLs and remove trailing slashes
+      return new URL(value, location.href).href.replace(/\/$/, '')
+    }),
   },
   runtimeEnv: {
     VITE_API_BASE_URL: import.meta.env.VITE_API_BASE_URL,
