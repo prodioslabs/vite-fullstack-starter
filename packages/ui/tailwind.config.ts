7981837188ca25ad6eca967b0b6974eb97a47e69
nameerakhter <akhtarnameer@gmail.com>
2025-09-17 17:56:29 +0530

    fix(ui): remove ui dir fomr packages

--- packages/ui/tailwind.config.ts
+++
@@ -1,8 +0,0 @@
-import baseConfig, { TailwindConfig } from '@repo/config/tailwind.config'
-
-const config = {
-  ...baseConfig,
-  content: ['./src/**/*.{js,jsx,tsx,ts}', ...baseConfig.content],
-} satisfies TailwindConfig
-
-export default config
