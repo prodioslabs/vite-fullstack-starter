4029627dfcb68575042f53d683e9a7519aa13f12
nameerakhter <akhtarnameer@gmail.com>
2025-12-15 11:43:28 +0530

    feat(version.match.ts): update version match script

--- scripts/version.match.ts
+++ scripts/version.match.ts
@@ -1,5 +1,5 @@
-import { promises as fs } from "fs";
-import * as path from "path";
+import * as fs from "node:fs/promises";
+import * as path from "node:path";
 import { z } from "zod";
 
 const packagesDirs = ["packages", "apps"];
