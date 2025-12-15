ccae3d7440ebbbc538ec7d14ba3fb5588274e99c
nameerakhter <akhtarnameer@gmail.com>
2025-12-15 11:39:02 +0530

    refactor(theme-provider, theme-toggle): remove unused

--- apps/landing-page/src/app/[lang]/_components/theme-toggle.tsx
+++ apps/landing-page/src/app/[lang]/_components/theme-toggle.tsx
@@ -1,6 +1,3 @@
-'use client'
-
-import * as React from 'react'
 import { MoonIcon, SunIcon } from 'lucide-react'
 import { motion, AnimatePresence } from 'framer-motion'
 import { useTheme } from 'next-themes'
