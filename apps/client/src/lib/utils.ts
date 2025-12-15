597dccbe5cc70f3a8a419d848fb0a8a13ae1131b
nameerakhter <akhtarnameer@gmail.com>
2025-12-15 15:28:47 +0530

    feat(utils): add getErrorMessage utility

--- apps/client/src/lib/utils.ts
+++ apps/client/src/lib/utils.ts
@@ -1,6 +1,30 @@
+import { AxiosError } from 'axios'
 import { type ClassValue, clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+type ErrorWithMessage = {
+  error?: string
+  message?: string
+}
+
+export function getErrorMessage(error: unknown, defaultMessage: string = 'Something went wrong. Please try again.') {
+  let message = defaultMessage
+
+  if (error && typeof error === 'object') {
+    const errorObj = error as ErrorWithMessage
+
+    if (errorObj.error) {
+      message = errorObj.error
+    } else if (error instanceof AxiosError) {
+      message = error.response?.data?.message || error.message
+    } else if (error instanceof Error) {
+      message = error.message
+    }
+  }
+
+  return message
 }
