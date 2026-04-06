db30269f15890f5efa19a513322bff89b7b47f67
Abinash Panda <abinash@prodios.com>
2026-04-06 15:36:54 +0530

    chore: delete server app

--- apps/server/src/db/schema/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './auth'
-export * from './notification'
-export * from './file'
--- apps/server/src/utils/constants.ts
+++
@@ -1,8 +0,0 @@
-export const SECOND = 6000 // milliseconds
-export const MINUTE = SECOND * 60
-export const HOUR = MINUTE * 60
-export const DAY = HOUR * 24
-
-export const PASSWORD_RESET_TOKEN_EXPIRY = 15 * MINUTE // 15 minutes
-
-export const DEFAULT_JOB_TIMEOUT_MS = 5 * MINUTE // 5 minutes
