5ccbd63e5a6ce6a7eb7a8712d5821bb8cca6828b
Abinash Panda <abinash@prodios.com>
2026-04-06 15:36:14 +0530

    chore: remove contracts package

--- packages/contracts/prettier.config.js
+++
@@ -1,1 +0,0 @@
-export { default } from '@repo/config/prettier.config.js'
--- packages/contracts/src/contracts/index.ts
+++
@@ -1,9 +0,0 @@
-import { notificationContract } from './notification.contract'
-
-export * from './auth.contract'
-export * from './notification.contract'
-export * from './file.contract'
-
-export const contract = {
-  notifications: notificationContract,
-}
